478499892c12d30716177eb5dbb2d2d517d27e0c
AndreaLlerena2003 <20201173@aloe.ulima.edu.pe>
2024-10-15 15:20:29 -0500

    feat/kafka config

--- apps/auth-service/src/app/auth-manager/auth-manager.controller.ts
+++ apps/auth-service/src/app/auth-manager/auth-manager.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, HttpCode, HttpStatus, UseGuards , Res} from '@nestjs/common';
+import { AuthManagerService } from './auth-manager.service';
+import { RegisterUserDto } from '../dto/register-user.dto';
+import { LoginUserDto } from '../dto/login-user.dto';
+import { LocalAuthGuard } from '../guards/local-auth.guard';
+import { CurrentUser } from './current-user.decorator';
+import { Response } from 'express';
 
 @Controller('auth-manager')
-export class AuthManagerController {}
+export class AuthManagerController {
+    constructor(private readonly authManagerService: AuthManagerService) {}
+
+    @Post('register')
+    @HttpCode(HttpStatus.CREATED)
+    async register(@Body() registerUserDto: RegisterUserDto) {
+        return this.authManagerService.registerUser(registerUserDto);
+    }
+
+    @UseGuards(LocalAuthGuard)
+    @Post('login')
+    @HttpCode(HttpStatus.OK)
+    async login(
+        @CurrentUser() user: LoginUserDto,
+        @Res({ passthrough: true }) response: Response,
+    ) {
+        await this.authManagerService.login(user, response);
+        response.send(user);
+    }
+}
--- apps/auth-service/src/app/auth-manager/auth-manager.module.ts
+++ apps/auth-service/src/app/auth-manager/auth-manager.module.ts
@@ -3,11 +3,26 @@ import { AuthManagerService } from './auth-manager.service';
 import { AuthManagerController } from './auth-manager.controller';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { AuthTokens, AuthUsers } from '@backend-in-studio/db-manager-auth';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { LocalStrategy } from '../strategies/local.strategy';
+import { JwtStrategy } from '../strategies/jwt.strategy';
+import { KafkaManagerModule} from '@backend-in-studio/kafka-manager';
 @Module({
   imports: [
+    JwtModule.registerAsync({
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: `${configService.get('JWT_EXPIRATION')}s`, 
+        },
+      }),
+      inject: [ConfigService],
+    }),
     SequelizeModule.forFeature([AuthTokens, AuthUsers]), 
+    KafkaManagerModule
   ],
-  providers: [AuthManagerService],
+  providers: [AuthManagerService, JwtStrategy, LocalStrategy],
   controllers: [AuthManagerController]
 })
 export class AuthManagerModule {}
