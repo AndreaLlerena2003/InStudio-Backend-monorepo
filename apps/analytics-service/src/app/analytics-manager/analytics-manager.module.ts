54c407a3d89943e77d41fa61d45172d8a80a6672
Eduardo Ramón <edu.rz@hotmail.com>
2024-11-09 19:04:23 -0500

    invoke lambda

--- apps/analytics-service/src/app/analytics-manager/analytics-manager.module.ts
+++ apps/analytics-service/src/app/analytics-manager/analytics-manager.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AnalyticsManagerController } from './analytics-manager.controller';
 import { AnalyticsManagerService } from './analytics-manager.service';
-import {Analytics} from '@backend-in-studio/db-manager-analytics';
-import { Booking_Status } from '@backend-in-studio/db-manager-analytics';
-import { SequelizeModule } from '@nestjs/sequelize';
+import { LambdaManagerAnalyticsModule } from '@backend-in-studio/lambda-manager-analytics';
 
 @Module({
   imports: [
-    SequelizeModule.forFeature([Analytics,Booking_Status]), 
+    LambdaManagerAnalyticsModule
   ],
   controllers: [AnalyticsManagerController],
   providers: [AnalyticsManagerService]
