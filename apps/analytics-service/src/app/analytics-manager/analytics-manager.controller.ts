23ce086cc764cc15a558ec413088e9b4f92f259d
Eduardo Ramón <edu.rz@hotmail.com>
2024-11-09 19:04:23 -0500

    invoke lambda

--- apps/analytics-service/src/app/analytics-manager/analytics-manager.controller.ts
+++ apps/analytics-service/src/app/analytics-manager/analytics-manager.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { BookingEventDto } from './dto/booking-event-dto';
+import { AnalyticsManagerService } from './analytics-manager.service';
 
 @Controller('analytics-manager')
-export class AnalyticsManagerController {}
+export class AnalyticsManagerController {
+  constructor(private readonly analyticsManagerService: AnalyticsManagerService) {}
+
+  @Get() 
+  helloWorld() {
+    return this.analyticsManagerService.helloWorld();
+  } 
+
+  @Post()
+  async processBookingEvent(@Body() bookingEventDto: BookingEventDto) {
+    const result = await this.analyticsManagerService.processBookingEvent
+    (bookingEventDto);
+
+    // En realidad no debería retornar nada, ya que es un evento
+    return {
+      message: 'Booking register created successfully',
+      data: result,
+    };
+  }
+}
--- apps/analytics-service/src/app/analytics-manager/analytics-manager.service.ts
+++ apps/analytics-service/src/app/analytics-manager/analytics-manager.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
-
+import { BookingEventDto } from './dto/booking-event-dto';
+import { LambdaService } from 'libs/lambda-manager-analytics/src/lib/lambda.service';
 @Injectable()
-export class AnalyticsManagerService {}
+export class AnalyticsManagerService {
+  constructor(private readonly lambdaService: LambdaService) {}
+  
+  helloWorld() {
+    return {
+      status: 200,
+      message: "Hello World 🙈"
+    }
+  }
+
+  processBookingEvent(bookingEventDto: BookingEventDto): {} {
+    this.lambdaService.invokeLambda(bookingEventDto)
+    return {
+      processedData: bookingEventDto,
+    };
+  }
+}
