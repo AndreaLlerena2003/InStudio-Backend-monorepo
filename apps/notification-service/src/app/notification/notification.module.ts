af3f077d9136aef80f7eed3267bcef7f01653ba4
AndreaLlerena2003 <20201173@aloe.ulima.edu.pe>
2024-11-11 23:27:42 -0500

    fix : kafka style

--- apps/notification-service/src/app/notification/notification.module.ts
+++ apps/notification-service/src/app/notification/notification.module.ts
@@ -1,25 +1,32 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { ClientsModule } from '@nestjs/microservices';
-import { kafkaConfig } from '../shared/kafka.config';
 import { NotificationController } from './notification.controller';
 import { NotificationService } from './notification.service';
 import { NotificationRepository } from './notification.repository';
 import { PriorityNotificationManager } from '../../services/priority-notification-manager';
 import { DynamooseManagerModule } from '@backend-in-studio/dynamoose-manager';
 import { KafkaManagerModule } from '@backend-in-studio/kafka-manager';
-
+import { ClientsModule, Transport } from '@nestjs/microservices';
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    ClientsModule.register([
-      {
-        name: 'KAFKA_SERVICE',
-        ...kafkaConfig,
-      },
-    ]),
     DynamooseManagerModule,
     KafkaManagerModule,
+    ClientsModule.register([
+      {
+        name: 'auth-client',
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            clientId: 'client-notification-service',
+            brokers: ['localhost:9092'],
+          },
+          consumer: {
+            groupId: 'backend-InStudio-auth-service',
+            allowAutoTopicCreation: true,
+          },
+        },
+      }])
   ],
   controllers: [NotificationController],
   providers: [
