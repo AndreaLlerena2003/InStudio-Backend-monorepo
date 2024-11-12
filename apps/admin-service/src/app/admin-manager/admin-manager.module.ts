62da4092fa88082ff78beb2f181442992d18bd07
AndreaLlerena2003 <20201173@aloe.ulima.edu.pe>
2024-11-11 22:57:51 -0500

    feat: admin manager

---
+++ apps/admin-service/src/app/admin-manager/admin-manager.module.ts
@@ -0,0 +1,36 @@
+import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { AdminManagerService } from './admin-manager.service';
+import { AdminManagerController } from './admin-manager.controller';
+import { Service, Salon, Subcategory , Admin} from '@backend-in-studio/db-manager-admin'
+import { KafkaManagerModule } from '@backend-in-studio/kafka-manager';
+import {AuthLibModule} from '@backend-in-studio/auth-lib';
+import {S3ManagerModule} from '@backend-in-studio/s3-manager';
+import { JwtAuthGuard } from '@backend-in-studio/auth-lib';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+@Module({
+  imports: [
+    SequelizeModule.forFeature([Service, Salon, Subcategory, Admin]), 
+    KafkaManagerModule,
+    S3ManagerModule,
+    AuthLibModule,
+    ClientsModule.register([
+      {
+        name: 'auth-client',
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            clientId: 'client-admin-service',
+            brokers: ['localhost:9092'],
+          },
+          consumer: {
+            groupId: 'backend-InStudio-auth-service',
+            allowAutoTopicCreation: true,
+          },
+        },
+      }])
+  ],
+  controllers: [AdminManagerController],
+  providers: [AdminManagerService,  JwtAuthGuard],
+})
+export class AdminManagerModule {}
--- apps/auth-service/src/app/auth-manager/auth-manager.service.ts
+++ apps/auth-service/src/app/auth-manager/auth-manager.service.ts
@@ -1,6 +1,7 @@
 import { InjectModel } from '@nestjs/sequelize';
 import { AuthUsers } from '@backend-in-studio/db-manager-auth';
 import { RegisterUserDto } from '../dto/register-user.dto';
+import { RegisterAdminDto } from '../dto/register-admin.dto';
 import {
     Injectable,
     UnauthorizedException,
@@ -8,131 +9,143 @@ import {
   } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
 import {JwtService} from '@nestjs/jwt';
-
-export interface TokenPayload {
-    userId: string;
-}
 import { Response } from 'express';
 import { ConfigService } from '@nestjs/config';
 import { v4 as uuidv4 } from 'uuid'; 
 import { KafkaService } from 'libs/kafka-manager/src/lib/kafka-service';
 
+export interface TokenPayload {
+    userId: string;
+    role: number; 
+}
+
 @Injectable()
 export class AuthManagerService {
     constructor(
         @InjectModel(AuthUsers)
-        private readonly authService : typeof AuthUsers,
+        private readonly authService: typeof AuthUsers,
         private readonly jwtService: JwtService,
         private readonly configService: ConfigService,
         private readonly kafkaService: KafkaService,
-    ){
+    ) {
         kafkaService.init();
     }
 
-    async registerUser(registerUserDto: RegisterUserDto){
+    async registerUser(registerUserDto: RegisterUserDto) {
         await this.validateCreateUserRequest(registerUserDto);
         const hashedPassword = await bcrypt.hash(registerUserDto.password, 10);
         const external_id = uuidv4();
-        console.log(`Password before hashing: ${registerUserDto.password}`);
+
         const newUser = await this.authService.create({
             email: registerUserDto.email,
             password: hashedPassword,
-            role: 0,
-            external_id: external_id
+            role: 0, // User role
+            external_id,
         });
-        const userRegistered = {
-            id: external_id,
-            name: registerUserDto.name,
-            profile_photo_url: registerUserDto.profile_photo_url,
-            districtId: registerUserDto.districtId
-        }
-        this.kafkaService.sendEvent(userRegistered,'userRegistered');
+
+        this.kafkaService.sendEvent(
+            {
+                id: external_id,
+                name: registerUserDto.name,
+                profile_photo_url: registerUserDto.profile_photo_url,
+                districtId: registerUserDto.districtId,
+            },
+            'userRegistered',
+        );
+
         return {
             message: 'User registered successfully',
-            userId: newUser.external_id
+            userId: newUser.external_id,
         };
     }
-    async login(user: AuthUsers, response: Response){
+
+    async registerAdmin(registerAdminDto: RegisterAdminDto) {
+        await this.validateCreateUserRequest(registerAdminDto);
+        const hashedPassword = await bcrypt.hash(registerAdminDto.password, 10);
+        const external_id = uuidv4();
+
+        const newAdmin = await this.authService.create({
+            email: registerAdminDto.email,
+            password: hashedPassword,
+            role: 1, 
+            external_id,
+        });
+
+        this.kafkaService.sendEvent(
+            {
+                id: external_id,
+                name: registerAdminDto.name,
+                profile_photo_url: registerAdminDto.profile_photo_url,
+            },
+            'adminRegistered',
+        );
+
+        return {
+            message: 'Admin registered successfully',
+            adminId: newAdmin.external_id,
+        };
+    }
+
+    async login(user: AuthUsers, response: Response) {
         const tokenPayload: TokenPayload = {
             userId: user.external_id,
+            role: user.role,
         };
         const expires = new Date();
         expires.setSeconds(
             expires.getSeconds() + this.configService.get('JWT_EXPIRATION'),
-        ); 
+        );
+
         const token = this.jwtService.sign(tokenPayload);
-        response.cookie('Authentication', token, { 
-            httpOnly: true, 
-            expires, 
+        response.cookie('Authentication', token, {
+            httpOnly: true,
+            expires,
         });
     }
 
-    private async validateCreateUserRequest(request: RegisterUserDto) {
-        let user: AuthUsers;
-        try {
-          user = await this.authService.findOne({
-            where: {
-                email: request.email
-            }
-          });
-        } catch (err) {}
-    
+    private async validateCreateUserRequest(request: RegisterUserDto | RegisterAdminDto) {
+        const user = await this.authService.findOne({
+            where: { email: request.email },
+        });
         if (user) {
-          throw new UnprocessableEntityException('Email already exists.');
+            throw new UnprocessableEntityException('Email already exists.');
         }
     }
-    
+
     async validateUser(email: string, password: string) {
-        console.log(`Validating user with email: ${email}`); 
-        const user = await this.authService.findOne({ 
-            where: {
-                email: email
-            }
+        const user = await this.authService.findOne({
+            where: { email },
         });
-        if (!user) {
-            console.log(`No user found with email: ${email}`); 
-            throw new UnauthorizedException('User not found.');
+        if (!user || !(await bcrypt.compare(password.trim(), user.password))) {
+            throw new UnauthorizedException('Invalid credentials.');
         }
-        console.log(`User found: ${user.password}`);
-        const passwordIsValid = await bcrypt.compare(password.trim(), user.password);
-        if (!passwordIsValid) {
-            console.log(`Invalid password for user: ${email}`); 
-            throw new UnauthorizedException('Credentials are not valid.');
-        }
-    
-        console.log(`Password valid for user: ${email}`); 
         return user;
     }
 
-    async getUser(external_id: string) {
-        const user = await this.authService.findOne({ 
-            where: {
-                external_id: external_id
-            }
-         });
-        
-        return user;    
+    async getUserByExternalId(external_id: string) {
+        return this.authService.findOne({
+            where: { external_id },
+        });
     }
 
-
-    async validateUserByToken(data: { Authentication: string }) {
+    async validateToken(data: { Authentication: string }, requiredRole?: number) {
         const { Authentication } = data;
         let payload: TokenPayload;
         try {
             payload = this.jwtService.verify(Authentication);
-        } catch (error) {
+        } catch {
             throw new UnauthorizedException('Invalid token');
         }
-        const { userId } = payload;
-        if (!userId) {
-            throw new UnauthorizedException('Invalid token payload');
-        }
-        const user = await this.getUser(userId);
+
+        const { userId, role } = payload;
+        const user = await this.getUserByExternalId(userId);
         if (!user) {
             throw new UnauthorizedException('User not found');
         }
-        return {user_id:userId}; 
+
+        return { userId, role };
     }
 }
+
 
 
