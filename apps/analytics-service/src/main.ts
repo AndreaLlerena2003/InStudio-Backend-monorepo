8258c96129eeda42fe817fb7110c3c85dee87b65
Eduardo Ramón <edu.rz@hotmail.com>
2024-11-12 18:51:53 -0500

    adding cors

--- apps/analytics-service/src/main.ts
+++ apps/analytics-service/src/main.ts
@@ -14,7 +14,8 @@ async function bootstrap() {
 
   app.setGlobalPrefix(globalPrefix);
   app.useGlobalPipes(new ValidationPipe());
-
+  app.enableCors();
+  
   const port = process.env.PORT || 3000;
   await app.listen(port);
   
