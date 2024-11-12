bcefcd37d7db74f7a09c5d714507dbfc302c6576
AndreaLlerena2003 <20201173@aloe.ulima.edu.pe>
2024-11-11 22:57:51 -0500

    feat: admin manager

---
+++ apps/admin-service/src/app/dto/create-salon-dto.ts
@@ -0,0 +1,32 @@
+import { IsString, IsNotEmpty, IsOptional, IsNumber } from 'class-validator';
+
+export class CreateSalonDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  location: string;
+
+  @IsString()
+  @IsNotEmpty()
+  phone: string;
+
+  @IsString()
+  @IsNotEmpty()
+  description: string;
+
+  @IsString()
+  @IsOptional()
+  profile_photo_url: string;
+
+  @IsString()
+  @IsOptional()
+  banner_photos_url: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  adminId: number; 
+
+}
