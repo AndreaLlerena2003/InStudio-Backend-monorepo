2e9166a1332cfdd54d22cd40ef003d271bc11c6b
AndreaLlerena2003 <20201173@aloe.ulima.edu.pe>
2024-11-17 00:07:59 -0500

    chore: refactor common module import

--- apps/admin-service/src/app/subcategory-manager/subcategory-manager.module.ts
+++ apps/admin-service/src/app/subcategory-manager/subcategory-manager.module.ts
@@ -8,27 +8,10 @@ import {AuthLibModule} from '@backend-in-studio/auth-lib';
 import {S3ManagerModule} from '@backend-in-studio/s3-manager';
 import { JwtAuthGuard } from '@backend-in-studio/auth-lib';
 import { ClientsModule, Transport } from '@nestjs/microservices';
+import { SharedModule } from '../shared/shared.module';
 @Module({
   imports: [
-    SequelizeModule.forFeature([Service, Salon, Subcategory, Admin]), 
-    KafkaManagerModule,
-    S3ManagerModule,
-    AuthLibModule,
-    ClientsModule.register([
-      {
-        name: 'auth-client',
-        transport: Transport.KAFKA,
-        options: {
-          client: {
-            clientId: 'client-admin-service',
-            brokers: ['localhost:9092'],
-          },
-          consumer: {
-            groupId: 'backend-InStudio-auth-service',
-            allowAutoTopicCreation: true,
-          },
-        },
-      }])
+   SharedModule
   ],
   controllers: [SubcategoryManagerController],
   providers: [SubcategoryManagerService,  JwtAuthGuard],
