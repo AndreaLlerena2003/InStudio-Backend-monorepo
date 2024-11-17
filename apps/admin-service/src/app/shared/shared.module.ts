085b81a9ff97f6b0d3d93fbfd0b4ed38e824b20a
AndreaLlerena2003 <20201173@aloe.ulima.edu.pe>
2024-11-17 00:07:59 -0500

    chore: refactor common module import

---
+++ apps/admin-service/src/app/shared/shared.module.ts
@@ -0,0 +1,39 @@
+import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { KafkaManagerModule } from '@backend-in-studio/kafka-manager';
+import { S3ManagerModule } from '@backend-in-studio/s3-manager';
+import { AuthLibModule } from '@backend-in-studio/auth-lib';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { Service, Salon, Subcategory, Admin, Category } from '@backend-in-studio/db-manager-admin';
+@Module({
+  imports: [
+    SequelizeModule.forFeature([Service, Salon, Subcategory, Admin, Category]), 
+    KafkaManagerModule,
+    S3ManagerModule,
+    AuthLibModule,
+    ClientsModule.register([
+      {
+        name: 'auth-client',
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            clientId: 'client-admin-service',
+            brokers: ['localhost:9092'],
+          },
+          consumer: {
+            groupId: 'backend-InStudio-auth-service',
+            allowAutoTopicCreation: true,
+          },
+        },
+      },
+    ]),
+  ],
+  exports: [
+    KafkaManagerModule,
+    S3ManagerModule,
+    AuthLibModule,
+    ClientsModule, 
+    SequelizeModule
+  ],
+})
+export class SharedModule {}
