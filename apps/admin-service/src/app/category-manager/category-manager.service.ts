0ae6261cb6d60a917a7cab921770621dbefffe5d
AndreaLlerena2003 <20201173@aloe.ulima.edu.pe>
2024-11-26 21:05:49 -0500

    feat: fixes

--- apps/admin-service/src/app/category-manager/category-manager.service.ts
+++ apps/admin-service/src/app/category-manager/category-manager.service.ts
@@ -4,6 +4,7 @@ import { KafkaService } from 'libs/kafka-manager/src/lib/kafka-service';
 import { S3Service } from 'libs/s3-manager/src/lib/s3-manager.service';
 import { CreateCategoryDto } from '../dto/create-category-dto';
 import { Category } from '@backend-in-studio/db-manager-admin';
+import { Subcategory } from '@backend-in-studio/db-manager-admin';
 
 @Injectable()
 export class CategoryManagerService {
@@ -46,6 +47,29 @@ export class CategoryManagerService {
       throw new InternalServerErrorException('Failed to fetch categories');
     }
   }
+
+
+  async getAllCategoriesWithSubcategories(): Promise<Category[]> {
+    try {
+      const categories = await this.categoryService.findAll({
+        include: [{
+          model: Subcategory,
+          as: 'subcategories', 
+        }],
+      });
+  
+      if (categories.length === 0) {
+        throw new NotFoundException('No categories found');
+      }
+  
+      this.logger.log(`Fetched all categories with subcategories: ${categories.length} categories found`);
+      return categories;
+    } catch (error) {
+      this.logger.error('Error fetching categories with subcategories', error);
+      throw new InternalServerErrorException('Failed to fetch categories with subcategories');
+    }
+  }
+  
 
 
   
